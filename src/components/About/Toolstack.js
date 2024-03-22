7784a597640f654d3d1d70fe6b7ed46dc4eb7b53
gaurav660 <55147756+gaurav660@users.noreply.github.com>
2024-03-22 16:09:28 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -2,23 +2,23 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
-  SiPostman,
+  SiGithub,
   SiSlack,
   SiVercel,
-  SiMacos,
+  SiLinux,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiLinux />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
+        <SiGithub />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
