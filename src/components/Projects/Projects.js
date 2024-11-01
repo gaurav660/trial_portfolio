f4154bdd136fb9da350ce536952f7fcf4c954a56
gaurav660 <55147756+gaurav660@users.noreply.github.com>
2024-11-01 19:50:52 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,6 +21,17 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={portfolio}
+              isBlog={false}
+              title="Gaurav | Portfolio"
+              description="Designed and developed a website using React.js and hosted on vercel.com. It’s a multi-page website and well structured for maintainability and future updates."
+              ghLink="https://github.com/gaurav660/trial_portfolio"
+              demoLink="https://www.grvdev.online/"
+            />
+          </Col>
+            
           <Col md={4} className="project-card">
             <ProjectCard
               // imgPath={chatify}
@@ -28,20 +39,8 @@ function Projects() {
               // title="Chatify"
               // description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               description="Working on it."
-              // ghLink="https://github.com/soumyajit4419/Chatify"
+              // ghLink="https://github.com/"
               // demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              // imgPath={bitsOfCode}
-              // isBlog={false}
-              // title="Bits-0f-C0de"
-              // description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              description="Working on it."
-              // ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              // demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
@@ -52,8 +51,8 @@ function Projects() {
               // title="Editor.io"
               // description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               description="Working on it."
-              // ghLink="https://github.com/soumyajit4419/Editor.io"
-              // demoLink="https://editor.soumya-jit.tech/"              
+              // ghLink="https://github.com/"
+              // demoLink="https://"              
             />
           </Col>
 
@@ -64,8 +63,8 @@ function Projects() {
               // title="Plant AI"
               // description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
               description="Working on it."
-              // ghLink="https://github.com/soumyajit4419/Plant_AI"
-              // demoLink="https://plant49-ai.herokuapp.com/"
+              // ghLink="https://github.com/"
+              // demoLink="https://"
             />
           </Col>
 
@@ -76,7 +75,7 @@ function Projects() {
             //   title="Ai For Social Good"
             //   description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
             description="Working on it."
-            //   ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+            //   ghLink="https://github.com/"
             //   // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -90,7 +89,7 @@ function Projects() {
               // Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               description="Working on it."
               // ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              // // demoLink=""      <--------Please include a demo link here 
             />
           </Col>
         </Row>
